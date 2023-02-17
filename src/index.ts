9175fcb3f1f5210bc02e897a03de27a151a07a1d
Özgür Gökdemir <ozgurgokd@gmail.com>
2023-02-17 04:03:38 +0300

    feat: add main functionality

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,22 @@
-export function add(a: number, b: number) {
-  return a + b;
+type Value = string | boolean | null | undefined;
+type Input = string | Value[];
+
+function handleNode(node: Value) {
+  if (typeof node !== 'string') return [];
+  const string = node.trim();
+  return string.length > 0 ? string.split(/\s+/) : [];
 }
 
-export default add;
+function handleArray(array: Value[]) {
+  return array.reduce<string[]>((acc, node) => {
+    const result = handleNode(node);
+    return result.length > 0 ? [...acc, ...result] : acc;
+  }, []);
+}
+
+export function ext(input: Input) {
+  if (typeof input === 'string') return handleNode(input).join(' ');
+  return handleArray(input).join(' ');
+}
+
+export default ext;
